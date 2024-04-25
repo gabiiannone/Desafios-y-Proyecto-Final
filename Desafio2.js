88baf4d7bb3de475f7ad8cc7083714a62dc9e6b8
Gabriel Iannone <83989370+gabiiannone@users.noreply.github.com>
2024-04-24 23:41:47 -0300

    Se completa el desafio 2

--- Desafio2.js
+++ Desafio2.js
@@ -1,17 +1,17 @@
-//const fs = require("fs");
+
 
 import {promises as fs } from "fs"
 
 class ProductManager {
     constructor() {
-        this.path = "./product.json";
+        this.path = "./product.txt"
         this.products = [];
 
 
     }
 
     static id = 0;
-
+    //INGRESO UN NUEVO PRODUCTO CON SU ID CORRESPONDIENTE
     addProduct = async (title, description, price, url, code, stock) => {
 
         ProductManager.id++;
@@ -30,6 +30,8 @@ class ProductManager {
 
         await fs.writeFile(this.path, JSON.stringify(this.products));
 
+        //console.log(newProduct);
+
     };
 
     readProducts = async () => {
@@ -42,20 +44,40 @@ class ProductManager {
 
         return console.log(respuesta2);
     };
-
+    //GENERO FUNCION ASINCRONA PARA BUSCAR PRODUCTO POR SU ID
     getProductsById = async (id) => {
 
         let respuesta3 = await this.readProducts();
-        if (!respuesta3.find(product => product.id === id)) {
+        
+        if (!respuesta3.find((product) => product.id === id)) {
             console.log("Product not found");
         } else {
-
-            console.log(filter);
+            console.log(respuesta3.find((product) => product.id === id));
         }
 
     };
 
+    //ELIMINO UN PRODUCTO CON EL ID QUE LE PASO
 
+    deleteProductsById = async (id) => {
+        let respuesta3 = await this.readProducts();
+        let productFilter = respuesta3.filter(products => products.id != id)
+        await fs.writeFile(this.path, JSON.stringify(productFilter));
+        //console.log("Product deleted successfully")
+    }
+
+    //MODIFICO UN PRODUCTO SIN CAMBIAR SU ID
+
+    updateProducts = async ({id, ...producto}) => {
+        await this.deleteProductsById(id);
+        let productOld = await this.readProducts()
+        
+
+        let productModif = [
+            {...producto,id}, ...productOld ];
+
+            await fs.writeFile(this.path, JSON.stringify(productModif));
+    };
 
 
 }
@@ -63,105 +85,31 @@ class ProductManager {
 
 const productos = new ProductManager();
 
-/*productos.addProduct("Boca indumentaria","Camiseta Titular",140000,"https://www.bocashop.com.ar/camiseta-titular-authentic-23-24-manga-larga-titular/p",1,500)
-productos.addProduct("Boca indumentaria","Camiseta Lanzamiento",70000,"https://www.bocashop.com.ar/camiseta-tercer-uniforme-boca-24-25-hombre/p",2,1000)
-productos.addProduct("Boca indumentaria","Short", 38000, "https://www.bocashop.com.ar/short-itular-boca-jrs-23-24/p",3, 150)
-*/
-//productos.addProduct()
-//productos.getProducts();
+//productos.addProduct("Boca indumentaria","Camiseta Titular",140000,"https://www.bocashop.com.ar/camiseta-titular-authentic-23-24-manga-larga-titular/p",1,500)
+//productos.addProduct("Boca indumentaria","Camiseta Lanzamiento",70000,"https://www.bocashop.com.ar/camiseta-tercer-uniforme-boca-24-25-hombre/p",2,1000)
+//productos.addProduct("Boca indumentaria","Short", 38000, "https://www.bocashop.com.ar/short-itular-boca-jrs-23-24/p",3, 150)
+
+
+//productos.addProduct("Boca Accesorios", "Kit pampera", 50000, "https://www.bocashop.com.ar/kit-pampera-xeneize/p",4, 300);
+
+
+
+productos.getProducts();
 
 productos.getProductsById(1)
 
-
-/* async addProduct(title, description, price, url,code, stock) {
-        try {
-            const product = await this.getProducts(product);
-            product.push(title, description, price, url,code, stock);
-            await fs.promises.writeFile(this.path, JSON.stringify(product));
-            
-        } catch (error) {
-            console.log(error);
-            
-            
-        }
-
-        if (this.path.some(p => p.code === code)){
-            console.log(`El código: ${code} ya existe`);
-            return;
-
-        }
-
-        const product = {
-            id: this.#getMaxId() + 1,
-            title,
-            description,
-            price,
-            url,
-            code,
-            stock,
-
-        };
-        
-     }
+productos.deleteProductsById(2)
 
 
-     #getMaxId() {
-        let maxId = 0;
-        this.products.map((product) => { 
-        if (product.id > maxId) maxId = product.id;
-        });
-        return maxId;
-    }
-
-    async getProducts() {
-        try {
-            if (fs.existsSync(this.path)){
-                const products = await fs.promises.readFile(this.path, "utf8");
-                return JSON.parse(products);
-            } else return [];
-
-        } catch (error) {
-            
-        }
-        return this.products;
-    }
-
-   async getProductsById(productId) {
-        return this.products.find(p => p.id === productId);
-
-
-    }
-
-
-}
-
-
-
-const product1 = {
+//MODIFICO EL STOCK POR EJEMPLO
+productos.updateProducts({
     title: "Boca indumentaria",
-    description: "Camiseta Titular",
-    price: 140000,
-    url:"https://www.bocashop.com.ar/camiseta-titular-authentic-23-24-manga-larga-titular/p",
-    code: 1,
-    stock: 500
-  };
-  
-  const product2 = {
-    title: "Boca indumentaria",
-    description: "Camiseta Lanzamiento",
-    price: 70000,
-    url:"https://www.bocashop.com.ar/camiseta-tercer-uniforme-boca-24-25-hombre/p",
-    code: 2,
-    stock: 1500
-  };
-
-
-productManager.addProduct()
-
-
-//console.log(productManager.getProducts());
-//console.log(productManager.getProductsById(2));*/
-
-
+    description: "Short",
+    price: 38000,
+    url:  "https://www.bocashop.com.ar/short-itular-boca-jrs-23-24/p",
+    code: 3,
+    stock: 140,
+    id:3
+    })
 
 
